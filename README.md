a2b866fa9ac8c1d6e5dafe1c6351a9c420018955
donaldferguson <donff2@aol.com>
2021-11-01 10:26:19 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# docker-flask-sample
+# E6156 - Topic in SW Engineering: Cloud Computing docker-flask-sample
+
+## Overview
+
+This is a very, very simple example of how to
+1. Build and run a simple Flask web application
+2. Package with Docker
+3. Push to Docker Hub
+4. Pull onto an EC2 instance
+5. Execute on EC2
+
+## Foundation
+
+I followed a simple [online tutorial.](https://docs.docker.com/language/python/build-images/)
+
+
+
---
+++ scripts.sh
@@ -0,0 +1,13 @@
+
+# I ran the commands below one at a time in the project environment
+# docker build --tag docker-flask-sample .
+# docker tag docker-flask-sample:latest docker-flask-sample:v1.0.0
+# docker tag docker-flask-sample:latest docker.io/donff2j/docker-flask-sample
+# docker run -p 5000:5000 donff2j/docker-flask-sample
+# docker push docker.io/donff2j/docker-flask-sample
+
+# I ran the commands below on the EC2 console
+# docker pull donff2j/docker-flask-sample:latest
+
+
+
